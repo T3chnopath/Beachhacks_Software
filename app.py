1dc4c709fb0fbad7c4f8304147e9742dcec142ce
T3chnopath <josias.navasca@gmail.com>
2023-02-25 16:59:03 -0800

    Initial class implementation in app.py

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 from tkinter import *
-app = Tk()
-app.geometry("400x400")
-canvas = Canvas(app, bg='black')
-canvas.pack(anchor='nw', fill='both', expand=1)
 
-def get_x_and_y(event):
-    global lasx, lasy
-    lasx, lasy = event.x, event.y
+class App():
 
-def draw_smth(event):
-    global lasx, lasy
-    canvas.create_line((lasx, lasy, event.x, event.y), 
-                      fill='red', 
-                      width=2)
-    lasx, lasy = event.x, event.y
+    frame = None
+    app = None
+    canvas = None
 
-canvas.bind("<Button-1>", get_x_and_y)
-canvas.bind("<B1-Motion>", draw_smth)
+    def __init__(self, size, bg):
+        self.app = Tk()
+        self.app.geometry(size)
+        
+        self.canvas = Canvas(self.app, bg=bg)
+        self.canvas.pack(anchor="nw", fill="both", expand=1)
+    
+        self.canvas.bind("<Button-1>", self.get_x_and_y)
+        self.canvas.bind("<B1-Motion>", self.draw_smth)
+        
+        self.app.mainloop()
+
+    def get_x_and_y(self, event):
+        global lasx, lasy
+        lasx, lasy = event.x, event.y
+
+    def draw_smth(self, event):
+        global lasx, lasy
+        self.canvas.create_line((lasx, lasy, event.x, event.y), 
+                        fill='red', 
+                        width=2)
+        lasx, lasy = event.x, event.y
+
+App("400x400", "black")
