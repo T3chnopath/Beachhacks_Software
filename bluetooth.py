00507f2190f9e6d8c1d2337ffdf4f1815835414c
T3chnopath <josias.navasca@gmail.com>
2023-02-25 21:55:55 -0800

    Added sockets file

--- bluetooth.py
+++ bluetooth.py
@@ -1,10 +1,19 @@
+import serial
+
 class Bluetooth():
 
-    def __init__(self):
-        pass
+    ser = None
+
+    def __init__(self, COM, baudrate):
+        self.COM = COM
+        self.baudrate = baudrate
+
+        self.ser = serial.Serial(port=COM, baudrate=baudrate)
 
     def send(self, shapes):
         for shape in shapes:
-            print("Shape!")
             for lines in shape.getVectors():
                 print(lines.angle, lines.magnitude)
+                self.ser.write("test!")
+
+    
