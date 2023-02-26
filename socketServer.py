2fd662848d112ae276068615ed81ea046f718573
T3chnopath <josias.navasca@gmail.com>
2023-02-26 01:25:12 -0800

    Initial socket Server class

--- socketServer.py
+++ socketServer.py
@@ -1,72 +1,52 @@
-'''
-import socket
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server``:
-
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            print(data)
-
-class Server:
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-'''
-
-#!/usr/bin/python           # This is server.py file                                                                                                                                                                           
 
 import socket               # Import socket module
 from threading import Thread
 import threading
 
-clients = set()
-clients_lock = threading.Lock()
+HOST = "127.0.0.1" # Get local machine name
+PORT = 65432                # Reserve a port for your service.
 
-def on_new_client(client,addr):
-    '''
-    while True:
-        msg = clientsocket.recv(1024)
-        #do some checks and if msg == someWeirdSignal: break:
-        #Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
-        clientsocket.send(msg)
-    clientsocket.close()
-    '''
-    with clients_lock:
-        clients.add(client)
-    try:    
-        while True:
-            data = client.recv(1024)
-            print(data)
-            if not data:
-                break
-            else:
+class Server():
+    clients = set()
+    clients_lock = threading.Lock()
+    server = socket.socket()
+    HOST = None
+    PORT = None
+
+    def __init__(self, host, port):
+        self.HOST = host
+        self.PORT = port
+        self.server.bind((host, port))
+        self.server.listen(5) 
+
+    def handler(self, client, address):
+        with self.clients_lock:
+            self.clients.add(client)
+            
+        try:   
+            while True:
+                data = client.recv(1024)
                 
-                with clients_lock:
-                    for c in clients:
-                        c.sendall(data)
-    finally:
-        with clients_lock:
-            clients.remove(client)
+                if not data:
+                    break
 
-s = socket.socket()         # Create a socket object
-host = "127.0.0.1" # Get local machine name
-port = 65432                # Reserve a port for your service.
+                else:
+                    print(data)
+                    
+        finally:
+            with self.clients_lock:
+                self.clients.remove(client)
+    
+    def getServer(self):
+        return self.server
 
-#print 'Server started!'
-#print 'Waiting for clients...'
+if __name__ == "__main__":
+    
+    server = Server(HOST, PORT)
+    thread = []
+    while True:
+        host, port = server.getServer().accept()
+        Thread(target=server.handler, args = (host,port)).start()
+    
+    server.getServer().close()
 
-s.bind((host, port))        # Bind to the port
-s.listen(5)                 # Now wait for client connection.
-
-#print 'Got connection from', addr
-while True:
-   host, port = s.accept()     # Establish connection with client.
-   #thread.start_new_thread(on_new_client,(c,addr))
-   Thread(target=on_new_client, args = (host,port)).start()
-   #Note it's (addr,) not (addr) because second parameter is a tuple
-   #Edit: (c,addr)
-   #that's how you pass arguments to functions when creating new threads using thread module.
-s.close()
