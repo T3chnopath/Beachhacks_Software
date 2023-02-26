611329aa0736055eee9edbf1fb3a2a015a658446
T3chnopath <josias.navasca@gmail.com>
2023-02-26 04:39:58 -0800

    Added both socket host IPs into main

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ CANVAS_SIZE = "400x400"
 CANVAS_COLOR = "black"
 CANVAS_TIME_RESOLUTION = 0.05
 
-SOCK_HOST = "192.168.43.128"  # The server's hostname or IP address
-SOCK_PORT = 65432  # The port used by the server
+SOCK_HOSTA = "192.168.43.128"  # The server's hostname or IP address
+SOCK_PORTA = 65432  # The port used by the server
+
+SOCK_HOSTB = "192.168.43.128"
+SOCK_HOSTB = 65432
 
 POLL_TIME = 1000
 
 if __name__ == "__main__":
     App(CANVAS_SIZE, CANVAS_COLOR, CANVAS_TIME_RESOLUTION, \
-        SOCK_HOST, SOCK_PORT, POLL_TIME)
+        SOCK_HOSTA, SOCK_PORTA, POLL_TIME)
