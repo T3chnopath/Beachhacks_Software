c997a91abe8edf6a0c9e1b3f6804f42bb48fbcc2
T3chnopath <josias.navasca@gmail.com>
2023-02-26 08:00:51 -0800

    ClientB implmentation

--- socketClientB.py
+++ socketClientB.py
@@ -1,7 +1,5 @@
 # echo-client.py
 import socket
-import time
-from multiprocessing import Value
 
 class Client():
     
@@ -11,25 +9,14 @@ class Client():
     def __init__(self, HOST, PORT):
         self.sock.connect((HOST, PORT))
 
-    def sendToServer(self, shapes):
-        finalMessage = []
-        for x in range(0, len(shapes)):
-            firstFlag = 1
-            message = []
-            for lines in shapes[x].getVectors():
-                
-                if firstFlag:
-                    temp = "[0, " + str(round(lines.angle, 3)), str(round(lines.magnitude, 3)) + "]"
-                    message.append(", ".join(temp))
-                    firstFlag = 0
-                else:
-                    temp = "[1, " + str(round(lines.angle, 3)), str(round(lines.magnitude, 3)) + "]"
-                    message.append(", ".join(temp))
-
-            byteMessage = bytes((''.join(message)), "utf-8")
-            self.sock.send(byteMessage)
-
     def receive(self):
         return(self.sock.recv(1024))
 
-   
+
+SOCK_HOSTB = "192.168.43.128"
+SOCK_PORTB = "65432"
+client = Client(SOCK_HOSTB, SOCK_PORTB)
+
+while True:
+    for x in str(client.receive):
+        print(x) 
